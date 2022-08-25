36a0d8dfab117a541886696d5f6073f33e9e5ed3
Leo Mancini <mancini@leo.gd>
2022-08-24 22:18:22 -0400

    Cleaning up

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,16 @@
 function getUserLocation() {
    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition( success, fail );
+      navigator.geolocation.getCurrentPosition(userLocationSucceeded, userLocationFailed);
    } else {
       alert('Browser does not support geolocation!');
    }
 }
 
-async function success(position) {
-   console.log(position.coords);
+async function userLocationSucceeded(position) {
    let riskLevelForUserLocation = await getRiskLevelForUserLocation(position);
 
    let backgroundColor;
-
+   
    switch(parseInt(riskLevelForUserLocation.CCL_community_burden_level_integer)) {
       case 0:
          backgroundColor = '#00F035';
@@ -36,28 +35,29 @@ async function success(position) {
    document.querySelector('h3').innerText = `Last updated ${lastUpdatedDate.toLocaleDateString('en-us', { weekday:'long', year:'numeric', month:'short', day:'numeric' })}`;
 }
 
+function userLocationFailed() {
+   console.error('Could not get user location!')
+}
+
 function parseDate(input) {
    let parts = input.split('-');
 
    return new Date(parts[0], parts[1]-1, parts[2]); // Note: months are 0-based
 }
 
-function fail() {
-   console.error('Could not get user location!')
-}
-
 async function getRiskLevelForUserLocation(userLocation) {
+   let url = 'https://jndaditnce62h556zs2k5q3kqu0hlvjr.lambda-url.us-east-1.on.aws/';
    let lat = userLocation.coords.latitude;
    let lng = userLocation.coords.longitude;
 
-   const response = await fetch(`https://jndaditnce62h556zs2k5q3kqu0hlvjr.lambda-url.us-east-1.on.aws/?location=${lat},${lng}`);
+   const response = await fetch(`${url}?location=${lat},${lng}`);
    const data = await response.json();
+
    return data;
 }
 
 async function initalize() {
    let userLocation = getUserLocation();
-
 }
 
 initalize();
